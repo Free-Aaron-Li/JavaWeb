df5105a341af9c33796a3a6dea59e4696f9a88a9
Aaron-Li <Free_Aaron.Li@outlook.com>
2022-07-09 11:34:51 +0800

    注解-了解并生成javadoc

---
+++ Reflect/src/annotation/AnnoDemo1.java
@@ -0,0 +1,25 @@
+package annotation;
+
+/**
+ * @projectName: JavaWeb
+ * @className: AnnoDemo1
+ * @link:
+ * @author: AaronLi
+ * @description: <p>文档注释</p>
+ * <p>注解Javadoc演示</p>
+ * @date: 2022/7/9 上午11:21
+ * @version: JDK17
+ * @since: 1.5
+ */
+public class AnnoDemo1 {
+
+    /**
+     * 计算两数和
+     *
+     * @param a
+     * @param b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
