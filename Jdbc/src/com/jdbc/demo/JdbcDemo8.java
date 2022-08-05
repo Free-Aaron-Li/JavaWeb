afccca115ccbb62cad193e5b67834385dc382bec
Free_Aaron.Li@outlook.com <Free_Aaron.Li@outlook.com>
2022-08-05 22:52:08 +0800

    更新

---
+++ Jdbc/src/com/jdbc/demo/JdbcDemo8.java
@@ -0,0 +1,25 @@
+package com.jdbc.demo;
+
+/**
+ * @projectName: JavaWeb_Workspace
+ * @className: JdbcDemo8
+ * @link:
+ * @author: AaronLi
+ * @description:
+ * <br>
+ * 练习
+ * <br>
+ * &emsp;需求：
+ * &emsp;<ol  type="1" start="1">
+ *     <li>通过键盘录入用户名和密码</li>
+ *     <li>判断用户是否登录成功</li>
+ * </ol>
+ * @date: 2022/8/5 下午10:32
+ * @version: JDK17
+ */
+public class JdbcDemo8 {
+    /**
+     * @date: 2022/8/5 下午10:45
+     * @description: 登录方法
+     */
+}
