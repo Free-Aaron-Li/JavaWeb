705b8e0013a67a0bb9a9ac3adf7504de2826c9d8
Free_Aaron.Li@outlook.com <Free_Aaron.Li@outlook.com>
2022-09-02 10:23:32 +0800

    变量

--- html/src/com/javascript/variable.js
+++ html/src/com/javascript/variable.js
@@ -7,30 +7,30 @@ var num2 = 1.2;
 var num3 = NaN;
 
 //输出到页面
-document.write(num+"<br>");
-document.write(num2+"<br>");
-document.write(num3+"<br>");
+document.write(num + "<br>");
+document.write(num2 + "<br>");
+document.write(num3 + "<br>");
 
 //判断数据类型
-document.write(num+"---"+typeof (num) + "<br>");
+document.write(num + "---" + typeof (num) + "<br>");
 
 //定义string类型
-var str="abc";
+var str = "abc";
 var str2 = 'edf';
 
-document.write(str+"<br>");
-document.write(str2+"<br>");
+document.write(str + "<br>");
+document.write(str2 + "<br>");
 
 //定义boolean类型
 var flag = true;
 
-document.write(flag+"<br>");
+document.write(flag + "<br>");
 
 //定义null，undefined
 var obj = null;
 var obj2 = undefined;
-var obj3 ;
+var obj3;
 
-document.write(obj+"<br>");
-document.write(obj2+"<br>");
-document.write(obj3+"<br>");
+document.write(obj + "<br>");
+document.write(obj2 + "<br>");
+document.write(obj3 + "<br>");
