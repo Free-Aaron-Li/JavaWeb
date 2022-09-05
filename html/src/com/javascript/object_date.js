9c32af0bbc7436b6e01e6c6cbd21b8bdc05957a6
Free_Aaron.Li@outlook.com <Free_Aaron.Li@outlook.com>
2022-09-05 11:35:27 +0800

    Date对象

--- html/src/com/javascript/object_date.js
+++ html/src/com/javascript/object_date.js
@@ -4,6 +4,7 @@
 *       var date = new Date();
 *   2、方法
 *       toLocaleString()：返回当前date对象对应的时间本地字符串格式
+*       getTime()：获取毫秒值。返回当前如期对象描述的时间到1970年1月1日零点毫秒值差
 * */
 
 var date = new Date();
@@ -11,3 +12,4 @@ document.write(date + "<br>");
 
 document.write(date.toLocaleString() + "<br>");
 document.write(date.getTimezoneOffset()+ "<br>" );
+document.write(date.getTime() + "<br>");
